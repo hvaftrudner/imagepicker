ddcb31ebf44feb9db9d7e0bebe86f47a00340b43
hvaftrudner <krill_eriksson@hotmail.com>
2020-09-27 22:56:31 +0200

    Imagepicker with camera function added

---
+++ project 10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  project 10
+//
+//  Created by Kristoffer Eriksson on 2020-09-26.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name : String
+    var image: String
+    
+    init(name: String, image: String){
+        self.name = name
+        self.image = image
+    }
+}
---
+++ project 10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  project 10
+//
+//  Created by Kristoffer Eriksson on 2020-09-25.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
